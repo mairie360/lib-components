b5152dea4ee767dc31e1788ca9501a1bdd421c7e
CarolinHugo <hugo.carolin@epitech.eu>
2025-07-08 17:45:48 +0400

    Feat: add tests folder for unit test

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
